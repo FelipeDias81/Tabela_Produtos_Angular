853bdc143784e0346af11b017b26c6d453e0079a
FelipeDias81 <felipedias6725@gmail.com>
2023-05-16 18:29:24 -0300

    tabela quase lá

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductComponent } from './product/product.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    id: number;
+    name: string;
+    quantidade: string;
+    validade: string;
+    preco: string;
+}
